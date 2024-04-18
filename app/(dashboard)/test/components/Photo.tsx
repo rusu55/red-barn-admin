2e282083577b04b7415880827f9d036800f30434
rusu55 <rusu55@outlook.com>
2024-04-17 21:38:32 -0500

    updated

---
+++ app/(dashboard)/test/components/Photo.tsx
@@ -0,0 +1,20 @@
+import React, { forwardRef } from "react";
+
+export const Photo = forwardRef(
+  ({ url, index, faded, style, ...props }: any, ref) => {
+    const inlineStyles = {
+      opacity: faded ? "0.2" : "1",
+      transformOrigin: "0 0",
+      height: index === 0 ? 410 : 200,
+      gridRowStart: index === 0 ? "span 2" : null,
+      gridColumnStart: index === 0 ? "span 2" : null,
+      backgroundImage: `url("${url}")`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundColor: "grey",
+      ...style,
+    };
+
+    return <div ref={ref} style={inlineStyles} {...props} />;
+  }
+);
---
+++ app/(dashboard)/test/components/SortablePhoto.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+import { Photo } from "./Photo";
+const SortablePhoto = (props: any) => {
+  const sortable = useSortable({ id: props.url });
+
+  const {
+    attributes,
+    listeners,
+    isDragging,
+    setNodeRef,
+    transform,
+    transition,
+  } = sortable;
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <Photo
+      ref={setNodeRef}
+      style={style}
+      {...props}
+      {...attributes}
+      {...listeners}
+    />
+  );
+};
+
+export default SortablePhoto;
