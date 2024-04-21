a9606dbec6dc65a740eeb316f1562e7fda4d0aa1
rusu55 <rusu55@yahoo.com>
2024-04-21 12:14:16 -0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {    
+const nextConfig = { 
+    basePath: '/admin',   
     images: {
         domains: ['files.edgestore.dev']
       },
