ac9843f5c4d813c4b485506f23a688f37edfbc19
rusu55 <rusu55@yahoo.com>
2024-04-24 12:07:02 -0500

    updated

--- components/nav/UserNav.tsx
+++ components/nav/UserNav.tsx
@@ -65,7 +65,7 @@ export function UserNav({ label }: any) {
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem
-          onClick={() => signOut({ callbackUrl: "/blogs", redirect: true })}
+          onClick={() => signOut({ callbackUrl: "http://admin.redbarnweddingstudio.xyz", redirect: true })}
         >
           Log out
         </DropdownMenuItem>
